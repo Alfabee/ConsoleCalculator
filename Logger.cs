9c60bac7af829f55bf9c1477fbdf5283188da9a6
Alfabee <alfafaza686@gmail.com>
2025-05-25 14:57:24 +0300

    feat: додано логування через ILogger

---
+++ Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Logger : ILogger
+    {
+        private readonly string logFile = "log.txt";
+
+        public void Log(string message)
+        {
+            File.AppendAllText(logFile, $"{DateTime.Now}: {message}\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleCalculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICalculator calc = new Calculator();
+        ILogger logger = new Logger();
+
+        Console.WriteLine("Введіть перше число:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+        int b = int.Parse(Console.ReadLine());
+
+
+        int sum = calc.Add(a, b);
+        Console.WriteLine($"Сума: {sum}");
+        logger.Log($"Add: {a} + {b} = {sum}");
+
+        int diff = calc.Subtract(a, b);
+        Console.WriteLine($"Різниця: {diff}");
+        logger.Log($"Subtract: {a} - {b} = {diff}");
+
+        int prod = calc.Multiply(a, b);
+        Console.WriteLine($"Добуток: {prod}");
+        logger.Log($"Multiply: {a} * {b} = {prod}");
+
+        int quot = calc.Divide(a, b);
+        Console.WriteLine($"Частка: {quot}");
+        logger.Log($"Divide: {a} / {b} = {quot}");
+
+    }
+}
